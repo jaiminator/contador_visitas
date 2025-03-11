bb6c1e44182a859068aed1101524d10d8b1d9969
Jaime Lopez <jaimelopezmislata@gmail.com>
2025-03-11 20:38:16 +0100

    Mejoras y con comentarios

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
+//OBTENEMOS EL SPAN CON EL ID 'contadorVisitas'
 const textoVisitas = document.getElementById('contadorVisitas');
 
+//ASIGNAMOS 'VISITAS' CON EL VALOR OBTENIDO DE LA CLAVE 'views'
 let visitas = localStorage.getItem('views');
 
+//OBTENEMOS Y ASIGNAMOS EVENTO AL BOTÓN PARA REESTABLECER EL CONTADOR DE VISITAS
 const btnReestablecer = document.getElementById('btnReestablecer');
 btnReestablecer.addEventListener('click', resetVisits);
 
+//SI HAY VISITAS
 if(visitas){
-    visitas = Number(visitas)+1
-    localStorage.setItem('views', visitas);
-    textoVisitas.innerText = visitas;
+    visitas++; //INCREMENTAMOS LAS VISITAS EN 1
+    localStorage.setItem('views', visitas);  //ACTUALIZAMOS EL VALOR DE LA CLAVE 'views'
+    textoVisitas.innerText = visitas;   //IMPRIMIMOS POR PANTALLA EL NÚMERO TOTAL DE VISITAS
 } else {
     localStorage.setItem('views', 1);
     textoVisitas.innerText = 1;
 }
 
+//FUNCIÓN PARA BORRAR LAS CLAVE 'views' Y REESTABLECER EL Nº DE VISITAS A 0
 function resetVisits() {
     localStorage.clear('views');
     textoVisitas.innerText = 0;
 }
 
-console.log(localStorage.getItem('views'));
+console.log(localStorage.getItem('views')); //MUESTRA EL ITEM 'views' POR CONSOLA
